ad32caa8a3a011f0770734cef28b47d9360af217
pushup666 <1149843835@qq.com>
2019-09-16 17:27:34 +0800

    Update Txt2Pdf.cs

--- Txt2PdfConvert/Txt2PdfConvert/Txt2Pdf.cs
+++ Txt2PdfConvert/Txt2PdfConvert/Txt2Pdf.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
@@ -123,32 +124,37 @@ namespace Txt2PdfConvert
                 if (fileName != null)
                 {
                     var baseFont = BaseFont.CreateFont(@"C:\Windows\Fonts\msyhbd.ttc,0", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-                    Font font; 
+                    Font font, titleFont, chapterFont, textFont;
                     Document document;
-                    
+                    float textFontSize;
+
                     switch (comboBoxDevice.Text)
                     {
                         case "iPad Mini2":
-                            font = new Font(baseFont, 36f);                             //iPad mini2 326 ppi
+                            textFontSize = 36f;                                                 //iPad mini2 326 ppi
                             document = new Document(new Rectangle(1536, 2048));         //iPad mini2 2048x1536
                             break;
                         case "iPad Pro 11":
-                            font = new Font(baseFont, 36f);                             //iPad Pro 11 264 ppi
+                            textFontSize = 36f;                                                 //iPad Pro 11 264 ppi
                             document = new Document(new Rectangle(1668, 2388));         //iPad Pro 11 2388x1668
                             break;
                         case "iPhone 5s":
-                            font = new Font(baseFont, 32f);                             //iPhone 5s 264 ppi
+                            textFontSize = 32f;                                                 //iPhone 5s 264 ppi
                             document = new Document(new Rectangle(640, 1136));          //iPhone 5s 1136x640
                             break;
                         case "iPhone 8":
-                            font = new Font(baseFont, 32f);                             //iPhone 8 264 ppi
+                            textFontSize = 32f;                                                 //iPhone 8 264 ppi
                             document = new Document(new Rectangle(750, 1334));          //iPhone 8 1334x750
                             break;
                         default:
-                            font = new Font(baseFont, 32f);                             //iPad Pro 11 264 ppi
+                            textFontSize = 36f;                                                 //iPad Pro 11 264 ppi
                             document = new Document(new Rectangle(1668, 2388));         //iPad Pro 11 2388x1668
                             break;
                     }
+
+                    textFont = new Font(baseFont, (float)(textFontSize * 1.0));
+                    chapterFont = new Font(baseFont, (float)(textFontSize * 1.5));
+                    titleFont = new Font(baseFont, (float)(textFontSize * 2.0));
 
                     //反色
                     //font.Color = BaseColor.WHITE;
@@ -164,6 +170,21 @@ namespace Txt2PdfConvert
                         string line;
                         while ((line = sr.ReadLine()) != null)
                         {
+                            if (line.StartsWith("<TITLE>"))
+                            {
+                                font = titleFont;
+                                line = line.Remove(0, 7);
+                            }
+                            else if (line.StartsWith("<CHAPTER>"))
+                            {
+                                font = chapterFont;
+                                line = line.Remove(0, 9);
+                            }
+                            else
+                            {
+                                font = textFont;
+                            }
+
                             document.Add(new Paragraph(line == "" ? "\n" : line.Trim(), font));
                         }
                     }
