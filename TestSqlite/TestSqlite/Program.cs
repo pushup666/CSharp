516a1fba9486e8bf8d83f8236c288b2432de723d
pushup666 <1149843835@qq.com>
2019-09-17 14:30:58 +0800

    1

---
+++ TestSqlite/TestSqlite/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = SqliteHelper.ExecuteScalar("SELECT Name FROM Test;",null).ToString();
+            Console.WriteLine(name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestSqlite/TestSqlite/SqliteHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TestSqlite
+{
+    static class SqliteHelper
+    {
+        private static readonly string connStr = @"Data Source = C:\Users\ssf\Desktop\sqlite\db\cs.db";
+
+
+        public static int ExecuteNonQuery(string sql, params SQLiteParameter[] pms)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(connStr))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    con.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string sql, params SQLiteParameter[] pms)
+        {
+            try
+            {
+                using (SQLiteConnection con = new SQLiteConnection(connStr))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, con))
+                    {
+                        if (pms != null)
+                        {
+                            cmd.Parameters.AddRange(pms);
+                        }
+                        con.Open();
+                        return cmd.ExecuteScalar();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "";
+            }
+            
+        }
+
+        public static SQLiteDataReader ExecuteReader(string sql, params SQLiteParameter[] pms)
+        {
+            SQLiteConnection con = new SQLiteConnection(connStr);
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, con))
+            {
+                if (pms != null)
+                {
+                    cmd.Parameters.AddRange(pms);
+                }
+                try
+                {
+                    con.Open();
+                    return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    //System.Data.CommandBehavior.CloseConnection,表示DataReader对象调用结束后，释放conn
+                }
+                catch
+                {
+                    con.Close();
+                    con.Dispose();
+                    throw;
+                }
+            }
+        }
+
+        public static DataTable ExecuteDataTable(string sql, params SQLiteParameter[] pms)
+        {
+            DataTable dt = new DataTable();
+            using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, connStr))
+            {
+                if (pms != null)
+                {
+                    adapter.SelectCommand.Parameters.AddRange(pms);
+                }
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+    }
+}
