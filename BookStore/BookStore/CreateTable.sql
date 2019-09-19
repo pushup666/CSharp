bab3757a1d0e73826585df74d2d2b22286e1f986
pushup666 <1149843835@qq.com>
2019-09-19 11:39:36 +0800

    Create CreateTable.sql

---
+++ BookStore/BookStore/CreateTable.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE Book (
+    UID        CHAR (32)     PRIMARY KEY
+                             NOT NULL,
+    Title      VARCHAR (100) NOT NULL,
+    Alias      VARCHAR (100),
+    Author     VARCHAR (100),
+    Note       VARCHAR (100),
+    Rate       INTEGER       DEFAULT (0) 
+                             NOT NULL,
+    DeleteFlag INTEGER       DEFAULT (0) 
+                             NOT NULL
+);
+
+
+CREATE TABLE Version (
+    UID           CHAR (32) PRIMARY KEY
+                            NOT NULL,
+    BookID        CHAR (32) NOT NULL
+                            REFERENCES Book (UID),
+    VersionNo     INTEGER   NOT NULL
+                            DEFAULT (1),
+    Content       TEXT      NOT NULL,
+    ContentHash   CHAR (32) NOT NULL,
+    ContentLength INTEGER   NOT NULL
+);
+
+
+CREATE TABLE Line (
+    UID       CHAR (32) PRIMARY KEY
+                        NOT NULL,
+    VersionID CHAR (32) REFERENCES Version (UID) 
+                        NOT NULL,
+    LineNo    INTEGER   NOT NULL
+                        DEFAULT (1),
+    Content   TEXT      NOT NULL
+);
