45cf7beaf9d8b73ef76d5e297cfe3031dafb623b
pushup666 <1149843835@qq.com>
2019-11-11 16:44:00 +0800

    Update Program.cs

--- TestNAudio/TestNAudio/Program.cs
+++ TestNAudio/TestNAudio/Program.cs
@@ -12,18 +12,24 @@ namespace TestNAudio
     {
         private static void Main(string[] args)
         {
-            const string audioFileName = @"D:\Music\2pac - Life Goes On.mp3";
+            //const string audioFileName = @"D:\Music\2pac - Life Goes On.mp3";
 
-            using (var audioFile = new AudioFileReader(audioFileName))
-            using (var outputDevice = new WaveOutEvent())
-            {
-                outputDevice.Init(audioFile);
-                outputDevice.Play();
-                while (outputDevice.PlaybackState == PlaybackState.Playing)
-                {
-                    Thread.Sleep(1000);
-                }
-            }
+            //using (var audioFile = new AudioFileReader(audioFileName))
+            //using (var outputDevice = new WaveOutEvent())
+            //{
+            //    outputDevice.Init(audioFile);
+            //    outputDevice.Play();
+            //    while (outputDevice.PlaybackState == PlaybackState.Playing)
+            //    {
+            //        Thread.Sleep(1000);
+            //    }
+            //}
+
+            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+            Directory.CreateDirectory(outputFolder);
+            var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+            var capture = new WasapiLoopbackCapture();
+            var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
         }
     }
 }
