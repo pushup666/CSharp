826bed7199bf5bfc0fcd42562cb7cedf174249f3
pushup666 <1149843835@qq.com>
2019-11-12 10:45:59 +0800

    Update Program.cs

--- TestNAudio/TestNAudio/Program.cs
+++ TestNAudio/TestNAudio/Program.cs
@@ -1,36 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using NAudio.Wave;
+using NAudio.Wave.Compression;
 
 namespace TestNAudio
 {
-    class Program
+    static class Program
     {
+        static Dictionary<double, string> _lrcDict = new Dictionary<double, string>();
+
         private static void Main(string[] args)
         {
-            //const string audioFileName = @"D:\Music\2pac - Life Goes On.mp3";
+            ReadLyricFile(@"D:\Music\叶倩文 - 秋去秋来.lrc");
+            PlayAudioFile(@"D:\Music\叶倩文 - 秋去秋来.mp3");
 
-            //using (var audioFile = new AudioFileReader(audioFileName))
-            //using (var outputDevice = new WaveOutEvent())
-            //{
-            //    outputDevice.Init(audioFile);
-            //    outputDevice.Play();
-            //    while (outputDevice.PlaybackState == PlaybackState.Playing)
-            //    {
-            //        Thread.Sleep(1000);
-            //    }
-            //}
 
-            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
-            Directory.CreateDirectory(outputFolder);
-            var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
-            var capture = new WasapiLoopbackCapture();
-            var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
+
+
+            //var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
+            //Directory.CreateDirectory(outputFolder);
+            //var outputFilePath = Path.Combine(outputFolder, "recorded.wav");
+            //var capture = new WasapiLoopbackCapture();
+            //var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
+        }
+
+        private static void PlayAudioFile(string fileFullName)
+        {
+            try
+            {
+                var st = new Stopwatch();
+
+                using (var audioFile = new AudioFileReader(fileFullName))
+                using (var outputDevice = new WaveOutEvent())
+                {
+                    outputDevice.Init(audioFile);
+                    st.Restart();
+                    outputDevice.Play();
+                    while (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Console.SetCursorPosition(0, 0);
+                        Console.WriteLine($"{Path.GetFileName(fileFullName)?.PadRight(30, ' ')}{st.Elapsed:hh\\:mm\\:ss}");
+
+
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void ReadLyricFile(string fileFullName)
+        {
+            using (var sr = new StreamReader(fileFullName, Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (!line.Contains("]"))
+                    {
+                        continue;
+                    }
+
+                    var temp = line.Split(']');
+
+                    var seq = temp.Last().Trim();
+                    if (seq == "")
+                    {
+                        continue;
+                    }
+
+                    for (var i = 0; i < temp.Length - 1; i++)
+                    {
+                        var tsTemp = "00:" + temp[i].Replace("[", "");
+                        var ts = TimeSpan.Parse(tsTemp).TotalMilliseconds;
+
+                        _lrcDict.Add(ts, seq);
+                    }
+                }
+            }
+        }
+
+        private static string GetLyric(int elapsedMilliseconds)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < _lrcDict.Count; i++)
+            {
+                
+            }
+
+
+
+
+            return sb.ToString();
         }
     }
 }
