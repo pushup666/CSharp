c0c9ffeb4f4c36d84e5333e28cee583c479fa8b2
pushup666 <1149843835@qq.com>
2019-12-09 11:31:08 +0800

    1

---
+++ ByteFreqAnalysis/ByteFreqAnalysis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ByteFreqAnalysis
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var file = File.ReadAllBytes(@"D:\VM\VPN_RAM.part05.rar");
+            var arr = new int[256];
+
+            foreach (var b in file)
+            {
+                arr[b]++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var a in arr)
+            {
+                sb.AppendLine(a.ToString());
+            }
+
+            File.WriteAllText(@"C:\Users\ssf\Desktop\新建文件夹\111.txt",sb.ToString());
+        }
+    }
+}
