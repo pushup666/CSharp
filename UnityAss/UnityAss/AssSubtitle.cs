987b767d4e8e35fc21d3f691efa7bfc4e1de5c0a
pushup666 <1149843835@qq.com>
2023-04-12 17:10:11 +0800

    1

--- UnityAss/UnityAss/AssSubtitle.cs
+++ UnityAss/UnityAss/AssSubtitle.cs
@@ -612,10 +612,10 @@ namespace UnityAss
             var secIndex = parts.Length - 2;
             var minIndex = parts.Length - 3;
             var hourIndex = parts.Length - 4;
-            this.Hour = hourIndex > 0 ? Convert.ToInt32(parts[hourIndex]) : 0;
-            this.Minute = minIndex > 0 ? Convert.ToInt32(parts[minIndex]) : 0;
-            this.Second = secIndex > 0 ? Convert.ToInt32(parts[secIndex]) : 0;
-            this.Millisecond = msIndex > 0 ? Convert.ToInt32((parts[msIndex] + "000").Substring(0, 3)) : 0;
+            this.Hour = hourIndex >= 0 ? Convert.ToInt32(parts[hourIndex]) : 0;
+            this.Minute = minIndex >= 0 ? Convert.ToInt32(parts[minIndex]) : 0;
+            this.Second = secIndex >= 0 ? Convert.ToInt32(parts[secIndex]) : 0;
+            this.Millisecond = msIndex >= 0 ? Convert.ToInt32((parts[msIndex] + "000").Substring(0, 3)) : 0;
         }
 
         public AssEventTime(int hour, int minute, int second, int millisecond)
