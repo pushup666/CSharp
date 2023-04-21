0eca81d78877a54494678e56fd39c04b1c1bcd08
pushup666 <1149843835@qq.com>
2023-04-21 08:57:40 +0800

    1

--- BiliSaveConsole/BiliSaveConsole/Program.cs
+++ BiliSaveConsole/BiliSaveConsole/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace BiliSaveConsole
@@ -13,23 +11,84 @@ namespace BiliSaveConsole
     {
         private static string pathLoad = @"C:\Users\ssf\Desktop\Videos\bilibili";
         private static string pathSave = @"C:\Users\ssf\Desktop\Video";
-        JsonSerializer serializer = new JsonSerializer();
+
 
         static void Main(string[] args)
         {
-            
-            string[] listItem = Directory.GetDirectories(pathLoad,"*", SearchOption.TopDirectoryOnly);
 
-            foreach (var itemID in listItem)
+            var listItem = Directory.GetDirectories(pathLoad, "*", SearchOption.TopDirectoryOnly);
+
+            foreach (var itemPath in listItem)
             {
-                string filePlayUrl = itemID + "\\.playurl";
-                if (File.Exists(filePlayUrl))
+                var filePlayUrl = $"{itemPath}\\.playurl";
+                var fileVideoInfo = $"{itemPath}\\.videoInfo";
+
+                if (!File.Exists(filePlayUrl)) continue;
+
+                var listM4s = Directory.GetFiles(itemPath, "*.m4s");
+
+                if (listM4s.Length != 2)
                 {
-                    
-                    Dictionary<string, object> json = (Dictionary<string, object>)serializer.DeserializeObject(JsonData);
-                    string firstKey = json.ElementAt(0).Key;
-                    string secondKey = json.ElementAt(1).Key;
+                    Console.WriteLine($@"{itemPath}中m4s文件数为不等于2");
+                    continue;
                 }
+
+                for (var i = 0; i < listM4s.Length; i++)
+                {
+                    CrackMp4File(listM4s[i], $"{pathSave}\\{i}.m4s");
+                }
+
+                using (var file = File.OpenText(fileVideoInfo))
+                {
+                    using (var reader = new JsonTextReader(file))
+                    {
+                        var o = (JObject)JToken.ReadFrom(reader);
+
+                        var fileDstMp4 = $"{pathSave}\\P{o["p"]}.{o["title"]}.mp4";
+
+                        var cmdArguments = $"-i \"{pathSave}\\0.m4s\" -i \"{pathSave}\\1.m4s\" -vcodec copy -acodec copy \"{fileDstMp4}\"";
+                        ExecCmd(cmdArguments);
+                    }
+                }
+            }
+            Console.WriteLine(@"Finish!");
+        }
+
+        private static void CrackMp4File(string srcFile, string dstFile)
+        {
+            var srcBytes = File.ReadAllBytes(srcFile);
+            var dstBytes = new byte[srcBytes.Length - 9];
+
+            for (var i = 0; i < dstBytes.Length; i++)
+            {
+                dstBytes[i] = srcBytes[i + 9];
+            }
+
+            File.WriteAllBytes(dstFile, dstBytes);
+        }
+
+        private static void ExecCmd(string cmdArguments)
+        {
+            try
+            {
+                var p = new Process
+                {
+                    StartInfo =
+                    {
+                        WorkingDirectory = pathSave,
+                        FileName = "ffmpeg",
+                        Arguments = cmdArguments,
+                        WindowStyle = ProcessWindowStyle.Normal
+                    }
+                };
+
+                p.Start();
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\r\n跟踪;" + ex.StackTrace);
             }
         }
     }
