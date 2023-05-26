c0a4288705d862cebbddcadd38b8564a2db66ae3
pushup666 <1149843835@qq.com>
2023-05-26 19:02:19 +0800

    Update BiliSaveMain.cs

--- BiliSave/BiliSave/BiliSaveMain.cs
+++ BiliSave/BiliSave/BiliSaveMain.cs
@@ -17,8 +17,8 @@ namespace BiliSave
 
         private void BiliSaveMain_Load(object sender, EventArgs e)
         {
-            textBoxPathLoad.Text = @"C:\Users\ssf\Desktop\Videos\bilibili";
-            textBoxPathSave.Text = @"C:\Users\ssf\Desktop\Videos";
+            textBoxPathLoad.Text = @"Z:\Temp";
+            textBoxPathSave.Text = @"Z:\";
         }
 
         private void ButtonLoad_Click(object sender, EventArgs e)
