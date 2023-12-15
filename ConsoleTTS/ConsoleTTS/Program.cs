a671429e769522cbf4e3b5933f56cf744692f945
pushup666 <1149843835@qq.com>
2023-12-15 14:14:23 +0800

    Update Program.cs

--- ConsoleTTS/ConsoleTTS/Program.cs
+++ ConsoleTTS/ConsoleTTS/Program.cs
@@ -8,20 +8,36 @@ namespace ConsoleTTS
 {
     static class Program
     {
-        private static void Main()
+        //ConsoleTTS -path txtPath
+        //ConsoleTTS -file txtFile
+        private static void Main(string[] args)
         {
-            var files = Directory.GetFiles(@"Z:\Temp", "*.txt", SearchOption.TopDirectoryOnly);
-            foreach (var file in files)
+            if (args.Length == 0)
             {
-                Text2Wav(file);
+                GenBatFile(Environment.CurrentDirectory);
             }
-            
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            if (args.Length == 2)
+            {
+                if (args[0] == "-path")
+                {
+                    GenBatFile(args[1]);
+                }
+                if (args[0] == "-file")
+                {
+                    Txt2Wav(args[1]);
+                }
+                else
+                {
+                    ShowHelp();
+                }
+            }
+            else
+            {
+                ShowHelp();
+            }
         }
 
-        private static void Text2Wav(string textFileName)
+        private static void Txt2Wav(string textFileName)
         {
             if (textFileName == null) return;
 
@@ -29,19 +45,43 @@ namespace ConsoleTTS
 
             using (var synth = new SpeechSynthesizer())
             {
-                 var text = File.ReadAllText(textFileName, Encoding.Default);
+                var text = File.ReadAllText(textFileName, Encoding.Default);
                 var builder = new PromptBuilder();
                 builder.AppendText(text);
 
                 var wavFileName = Path.ChangeExtension(textFileName, "wav");
 
                 synth.Rate = 5;
-                synth.SetOutputToWaveFile(wavFileName, new SpeechAudioFormatInfo(44100, AudioBitsPerSample.Sixteen, AudioChannel.Mono));
+                synth.SetOutputToWaveFile(wavFileName, new SpeechAudioFormatInfo(44100, AudioBitsPerSample.Sixteen, AudioChannel.Stereo));
                 synth.Speak(builder);
-
-                //var mSoundPlayer = new System.Media.SoundPlayer(wavFileName);
-                //mSoundPlayer.Play();
             }
+        }
+
+        private static void GenBatFile(string path)
+        {
+            var sb = new StringBuilder();
+            var files = Directory.GetFiles(path, "*.txt", SearchOption.TopDirectoryOnly);
+            foreach (var file in files)
+            {
+                var wavFileName = Path.ChangeExtension(file, "wav");
+                var mp3FileName = Path.ChangeExtension(file, "mp3");
+
+                sb.AppendFormat(@"ConsoleTTS -file ""{0}""", file);
+                sb.AppendLine();
+                sb.AppendFormat(@"ffmpeg -channel_layout stereo -i ""{0}"" -codec:a libmp3lame -q:a 9 ""{1}""", wavFileName, mp3FileName);
+                sb.AppendLine();
+                sb.AppendFormat(@"del ""{0}""", wavFileName);
+                sb.AppendLine();
+            }
+
+            File.WriteAllText(Environment.CurrentDirectory + "\\ssf.bat", sb.ToString(), Encoding.Default);
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine("ConsoleTTS -path txtPath     to gen bat file");
+            Console.WriteLine("OR");
+            Console.WriteLine("ConsoleTTS -file txtFile     to gen wav file");
         }
     }
 }
